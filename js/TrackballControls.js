984648a27bb1fbb63282be732a10e4fc745f13ab
Vilson Vieira <vilson@void.cc>
2014-04-07 19:06:22 -0300

    hack on trackball control to set pan as default than rotate, added coords from my thesis to clusters layout

--- js/TrackballControls.js
+++ js/TrackballControls.js
@@ -6,7 +6,7 @@
 THREE.TrackballControls = function ( object, domElement ) {
 
 	var _this = this;
-	var STATE = { NONE: -1, ROTATE: 0, ZOOM: 1, PAN: 2, TOUCH_ROTATE: 3, TOUCH_ZOOM: 4, TOUCH_PAN: 5 };
+	var STATE = { NONE: -1, ROTATE: 2, ZOOM: 1, PAN: 0, TOUCH_ROTATE: 5, TOUCH_ZOOM: 4, TOUCH_PAN: 3 };
 
 	this.object = object;
 	this.domElement = ( domElement !== undefined ) ? domElement : document;
